52911fc386448ab39c4fbf8261b448bc80ca5746
SHIVAM SHUKLA <102867740+itshukla@users.noreply.github.com>
2024-05-29 09:48:04 +0530

    commit

---
+++ Java Learning/object.java
@@ -0,0 +1,30 @@
+class calc{
+
+    int a ;
+    public int add(){
+        System.out.println("Addition");
+        return 0;
+    }
+}
+public class object {
+
+    public static void main(String[] args) {
+        
+        int num1 = 4;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+        calc c = new calc();
+        c.add();
+
+
+    }
+}
+
+
+//Object Oriented Programming 
+
+// Object - Properties and Behaviours
+
+// Class - Blueprint
