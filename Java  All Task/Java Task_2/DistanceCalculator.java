f5e010595a394ee75705cb4114590a0d6e0ba0b5
SHIVAM SHUKLA <102867740+itshukla@users.noreply.github.com>
2024-06-08 21:10:27 +0530

    commit

---
+++ Java  All Task/Java Task_2/DistanceCalculator.java
@@ -0,0 +1,19 @@
+public class DistanceCalculator {
+
+     static double calculateDistance(double x1, double y1, double x2, double y2) {
+        // Formula for distance between two points
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+
+    public static void main(String[] args) {
+
+        double x1 = 12.0;
+        double y1 = 10.0;
+        double x2 = 9.0;
+        double y2 = 8.0;
+
+        System.out.println(calculateDistance(x1, y1, x2, y2));
+    }
+}
+
